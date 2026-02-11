11e2ed650e0b28825f10b4b39761d77d9217da9c
dhananjayn875 <dhananjay.8d.kmbvm@gmail.com>
2026-02-11 18:49:13 +0530

    moving development to windows

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,18 @@ import Pricing from "./components/Pricing"
 import Testimonials from "./components/Testimonials"
 import Features from "./components/Features"
 import Header from "./components/Header"
-
 // eslint-disable-next-line no-unused-vars
 import reactLogo from './assets/react.svg'
 
-
-
-
-function App() {
-  return <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
-  <Header />
-  <Navbar />
-  <Hero /> 
-  <Features />
-  <Pricing />
-  <Testimonials />
-  <Footer />   
+export default function App() {
+  return (<div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+    <Navbar />
+    <Hero />
+    <Footer />
+    <Pricing />
+    <Testimonials />
+    <Features />
+    <Header />
   </div>
+  )
 }
-export default App
