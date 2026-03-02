d3b15d2a53808e757e0517a52b0d8cb323df81de
dhananjayn875 <dhananjay.8d.kmbvm@gmail.com>
2026-03-02 19:36:52 +0530

    rounded navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,7 @@ export default function Navbar() {
     const [mobileMenueIsOpen, setMobileMenueIsOpen] = useState(false)
 
     return (
-
-        <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20  backdrop-blur-sm border-b border-red-950/10">
+        <nav className="fixed rounded-2xl top-2 left-2 right-2 z-50 transition-all duration-300 bg-slate-950/20  backdrop-blur-sm border-b border-red-950/10">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">{/*padding 4 below small screen, above small-6, large and above-8*/}
                 <div className="flex items-center justify-between h-14 sm:h-16 md:h-20">{/*height 14 below small screen, above small-16, above medium-20*/}
 
@@ -15,17 +14,19 @@ export default function Navbar() {
                             <img src="/vite.svg" alt="Logo" className="h-6 w-6 sm:w-8 sm:h-8" />
                         </div>
                         <span className="text-lg sm:text-xl md:text-2xl font-medium">
-                            <span className="text-white">Code</span>
-                            <span className="text-blue-400">HUB</span>
+                            <span className="text-white">Dhananjay</span>
+
+                            <span className="text-blue-400"> N</span>
                         </span>
                     </div>
 
                     {/*Navigation Links */}
+
                     <div className="hidden md:flex justify-start items-center space-x-6 lg:space-x-8">
-                        <a href="#features" className="text-grey-300 hover:text-white text-sm lg:text-base">Features</a>
-                        <a href="#Pricing" className="text-grey-300 hover:text-white text-sm lg:text-base">Pricing</a>
-                        <a href="#Testimonials" className="text-grey-300 hover:text-white text-sm lg:text-base">Testimonials</a>
-                        <a href="#Contact" className="text-grey-300 hover:text-white text-sm lg:text-base">Contact</a>
+                        <a href="#Home" className="text-grey-300 hover:text-white text-sm lg:text-base">Home</a>
+                        <a href="#About" className="text-grey-300 hover:text-white text-sm lg:text-base">About</a>
+                        <a href="#Projects" className="text-grey-300 hover:text-white text-sm lg:text-base">Projects</a>
+                        <a href="#Resume" className="text-grey-300 hover:text-white text-sm lg:text-base">Resume</a>
                     </div>
                     <button
                         className="md:hidden text-gray-300 hover:text-white focus:outline-none"
